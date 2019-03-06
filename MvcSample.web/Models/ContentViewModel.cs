27c589e2ffa0589a901083df84b3987df65d1335
Linyefeilyft <173436256@qq.com>
2019-03-06 18:03:50 +0800

    创建项目

---
+++ MvcSample.web/Models/ContentViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MvcSample.web.Models{
+    public class ContentViewModel{
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+}
